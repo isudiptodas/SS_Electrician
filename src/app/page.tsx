a98103493019272f68605a7d97d25e6f31a6f9f7
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-05-11 20:48:21 +0530

    added home page animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,9 +28,9 @@ function page() {
   const [selectedService, setSelectedService] = useState("")
   const [dropdownOpen, setDropdownOpen] = useState(false)
   const [submitting, setSubmitting] = useState(false)
-  const[name, setName] = useState('');
-  const[contact, setContact] = useState('');
-  const[message, setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [contact, setContact] = useState('');
+  const [message, setMessage] = useState('');
   const len = testimonials.length
   const prev = () => setActive((p) => p === 0 ? len - 1 : p - 1)
   const next = () => setActive((p) => p === len - 1 ? 0 : p + 1)
@@ -98,14 +98,14 @@ function page() {
 
   const submitForm = async () => {
 
-    if(submitting) return;
+    if (submitting) return;
 
-    if(!name || !selectedService || !contact || !message){
+    if (!name || !selectedService || !contact || !message) {
       toast.error("All fields required");
       return;
     }
 
-    if(contact.length < 10){
+    if (contact.length < 10) {
       toast.error("Contact number should not be less than 10 digits");
       return;
     }
@@ -120,14 +120,14 @@ function page() {
         message: message.trim(),
       });
 
-      if(res.status === 200){
+      if (res.status === 200) {
         toast.dismiss(id);
         toast.success("Form submitted");
       }
     } catch (err: any) {
       toast.error("Problem sending email. Please try again later")
     }
-    finally{
+    finally {
       toast.dismiss(id);
       setName("");
       setSelectedService("");
@@ -141,68 +141,68 @@ function page() {
       <div className={`w-full h-auto flex flex-col justify-start items-center relative overflow-hidden`}>
 
         {/* navbar with logo */}
-        <nav className={`w-full z-40 fixed h-auto flex justify-center items-center gap-2 py-5 backdrop-blur-2xl bg-white/20`}>
+        <motion.nav initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.4, ease: "easeInOut" }} className={`w-full z-40 fixed h-auto flex justify-center items-center gap-2 py-5 backdrop-blur-2xl bg-white/20`}>
           <img src="/assets/logo.png" className={`h-7`} />
           <p className={`w-auto select-none text-lg`}>SS ELECTRICIAN</p>
-        </nav>
+        </motion.nav>
 
         {/* hero section */}
         <div className={`w-[95%] relative mt-20 h-auto rounded-2xl lg:rounded-3xl overflow-hidden`}>
-          <img src="/assets/hero-portrait.jpg" className={`z-10 md:hidden h-[70vh] object-cover w-full`} />
-          <img src="/assets/hero-landscape.jpg" className={`z-10 hidden md:block h-[70vh] object-cover w-full`} />
-          <div className={`w-full z-20 flex flex-col justify-end items-center xl:items-start gap-2 px-3 pb-10 absolute bottom-0 h-[80%] md:h-full bg-linear-to-t from-black via-[#000000a2] to-transparent`}>
-            <h1 className={`w-full select-none text-center xl:text-start text-3xl md:text-6xl xl:pl-5 tracking-tighter font-sans font-semibold text-white`}>Quality Electricial Work</h1>
-            <h1 className={`w-full select-none text-center xl:text-start text-3xl md:text-6xl xl:pl-5 tracking-tighter font-sans font-semibold text-white`}>At Your Doorsteps</h1>
-            <p className={`w-full px-3 md:px-6 mt-5 text-center text-white text-[12px] md:text-lg xl:text-start select-none`}>From TV repairs and LED light installation to complete house wiring, I help homes & businesses with electrical work that lasts.</p>
+          <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8, ease: "easeInOut" }} src="/assets/hero-portrait.jpg" className={`z-10 md:hidden h-[70vh] object-cover w-full`} />
+          <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8, ease: "easeInOut" }} src="/assets/hero-landscape.jpg" className={`z-10 hidden md:block h-[70vh] object-cover w-full`} />
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8, ease: "easeInOut" }} className={`w-full z-20 flex flex-col justify-end items-center xl:items-start gap-2 px-3 pb-10 absolute bottom-0 h-[80%] md:h-full bg-linear-to-t from-black via-[#000000a2] to-transparent`}>
+            <motion.h1 initial={{ filter: "blur(20px)" }} animate={{ filter: "blur(0px)" }} transition={{ duration: 1.3, ease: "easeInOut" }} className={`w-full select-none text-center xl:text-start text-3xl md:text-6xl xl:pl-5 tracking-tighter font-sans font-semibold text-white`}>Quality Electrical Work</motion.h1>
+            <motion.h1 initial={{ filter: "blur(20px)" }} animate={{ filter: "blur(0px)" }} transition={{ duration: 1.3, ease: "easeInOut" }} className={`w-full select-none text-center xl:text-start text-3xl md:text-6xl xl:pl-5 tracking-tighter font-sans font-semibold text-white`}>At Your Doorsteps</motion.h1>
+            <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1.5, ease: "easeInOut" }} className={`w-full px-3 md:px-6 mt-5 text-center text-white text-[12px] md:text-lg xl:text-start select-none`}>From TV repairs and LED light installation to complete house wiring, I help homes & businesses with electrical work that lasts.</motion.p>
 
             {/* call button */}
-            <div onClick={call} className={`w-auto mt-3 md:hidden flex justify-center items-center h-auto rounded-full p-0.5 bg-[#2f66ff]`}>
+            <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1.7, ease: "easeInOut" }} onClick={call} className={`w-auto mt-3 md:hidden flex justify-center items-center h-auto rounded-full p-0.5 bg-[#2f66ff]`}>
               <p className={`w-auto text-black bg-white px-3 py-2 text-sm select-none rounded-full`}>Book a call</p>
               <span className={`px-2 text-white`}><IoMdCall /></span>
-            </div>
+            </motion.div>
 
             {/* mail button */}
             <div onClick={mail} className={`w-auto xl:ml-5 mt-3 hidden md:flex justify-center items-center h-auto rounded-full p-0.5 bg-[#ff2f2f]`}>
               <p className={`w-auto text-black bg-white px-3 py-2 text-sm select-none rounded-full`}>Mail now</p>
               <span className={`px-3 text-white`}><IoMail /></span>
             </div>
-          </div>
+          </motion.div>
         </div>
 
         {/* google rating */}
-        <div className={`w-full flex justify-center items-center gap-2 py-10`}>
+        <motion.div initial={{ opacity: 0, filter: "blur(20px)" }} animate={{ opacity: 1, filter: "blur(0px)" }} transition={{ duration: 1.9, ease: "easeInOut" }} className={`w-full flex justify-center items-center gap-2 py-10`}>
           <span className={`text-5xl`}><FcGoogle /></span>
           <div className={`w-auto flex flex-col justify-center items-start`}>
             <p className={`text-black text-sm font-semibold`}>4.8 stars on Google</p>
             <span className={`flex text-sm text-yellow-400`}><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></span>
           </div>
-        </div>
+        </motion.div>
 
         {/* inro */}
         <div className={`w-full flex flex-col justify-start items-center py-6`}>
-          <h2 className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6 mb-5`}>ABOUT</h2>
-          <p className={`w-full lg:w-[80%] px-6 md:px-20 text-center text-[12px] md:text-sm xl:text-lg text-black`}>My name is Sanjib Das, and I have been helping families and businesses with electrical repairs and installations
+          <motion.h2 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5, duration: 2, ease: "easeInOut" }} className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6 mb-5`}>ABOUT</motion.h2>
+          <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.5, ease: "easeInOut" }} className={`w-full lg:w-[80%] px-6 md:px-20 text-center text-[12px] md:text-sm xl:text-lg text-black`}>My name is Sanjib Das, and I have been helping families and businesses with electrical repairs and installations
             for many years. Every job, big or small, is done with full attention, safety, and honesty. I believe good service brings
-            customers back and that is how this work has grown.</p>
+            customers back and that is how this work has grown.</motion.p>
         </div>
 
         {/* services provided */}
         <div className={`w-full flex flex-col justify-start items-center py-6`}>
-          <h2 className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6 mb-5`}>SERVICES</h2>
+          <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.5, ease: "easeInOut" }} className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6 mb-5`}>SERVICES</motion.h2>
           <div className={`w-full mt-4 px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-5`}>
             {services.map((item, index) => {
-              return <div key={index} className={`w-full hover:shadow-2xl duration-300 ease-in-out cursor-pointer rounded-2xl flex flex-col justify-start items-start px-3 pt-3 pb-5 border border-[#a6a6a6]`}>
+              return <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.5, ease: "easeInOut" }} key={index} className={`w-full hover:shadow-2xl duration-300 ease-in-out cursor-pointer rounded-2xl flex flex-col justify-start items-start px-3 pt-3 pb-5 border border-[#a6a6a6]`}>
                 <span className={`p-3 select-none rounded-xl border-2 bg-[#ecf2ff] border-[#4a89ff]`}>{item.icon}</span>
                 <p className={`w-full select-none text-start mt-3 text-black font-bold text-xl`}>{item.name}</p>
                 <p className={`w-full select-none text-start mt-1 text-black text-sm`}>{item.subtext}</p>
-              </div>
+              </motion.div>
             })}
           </div>
         </div>
 
         {/* testimonials */}
         <div className={`w-full flex flex-col justify-start items-center py-6`}>
-          <h2 className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6 mb-2`}>CLIENT REVIEWS</h2>
+          <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.5, ease: "easeInOut" }} className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6 mb-2`}>CLIENT REVIEWS</motion.h2>
           <div className={`w-full max-w-275 relative flex flex-col justify-center items-center`}>
             <div className={`relative h-72 md:h-60 flex justify-center items-center w-full`} style={{ perspective: 1400 }}>
               <motion.div drag="x" dragConstraints={{ left: 0, right: 0 }} dragElastic={0.18} dragMomentum={false} onDragEnd={handleDragEnd} className={`absolute inset-0 flex justify-center items-center`}>
@@ -238,8 +238,8 @@ function page() {
 
         {/* form */}
         <div className={`w-full flex flex-col justify-start items-center pt-6 pb-10`}>
-          <h2 className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6`}>BOOK A SERVICE</h2>
-          <p className={`w-full text-sm text-center px-6 mb-2 mt-1`}>Usually responds within few hours.</p>
+          <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.5, ease: "easeInOut" }} className={`w-full tracking-tighter text-center font-bold text-4xl xl:text-5xl px-6`}>BOOK A SERVICE</motion.h2>
+          <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 0.5, ease: "easeInOut" }} className={`w-full text-sm text-center px-6 mb-2 mt-1`}>Usually responds within few hours.</motion.p>
           <div className={`w-[95%] md:w-[60%] lg:w-[50%] xl:w-[40%] h-auto flex flex-col justify-start items-center gap-4 px-3 py-4`}>
             <input onChange={(e) => setName(e.target.value)} value={name} type="text" className={`w-full rounded-full bg-gray-200 px-4 py-3 outline-none text-sm`} placeholder="Enter your name*" />
             <input onChange={(e) => setContact(e.target.value)} value={contact} type="number" className={`w-full rounded-full bg-gray-200 px-4 py-3 outline-none text-sm`} placeholder="Enter your contact*" />
@@ -272,7 +272,7 @@ function page() {
           <Link href='/contact' className={`w-auto active:opacity-60 duration-200 ease-in-out text-xm text-black cursor-pointer font-semibold select-none`}>CONTACT</Link>
         </div>
         <div className={`w-full py-6 flex justify-center items-center`}>
-              <p className={`select-none w-full text-center text-black opacity-75 text-sm`}>&copy; 2026 SS ELECTRICIAN. All rights reserved.</p>
+          <p className={`select-none w-full text-center text-black opacity-75 text-sm`}>&copy; 2026 SS ELECTRICIAN. All rights reserved.</p>
         </div>
       </div>
     </>
